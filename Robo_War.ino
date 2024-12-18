89809eaab9bf6b4ab666cb32fc0622509cfaf490
aki-itself <akbaraliajmal524@gmail.com>
2024-12-18 16:24:14 +0500

    Add files via upload

--- Robo_War.ino
+++ Robo_War.ino
@@ -1,12 +1,13 @@
-// BTS7960 Pins for Left Motor
+// BTS7960 Left Motor
 #define L_R_EN 7
 #define L_L_EN 8
 #define L_RPWM 5
 #define L_LPWM 6
 
-#define WP 13
+// Weapon Command
+#define WP 3
 
-// BTS7960 Pins for Right Motor
+// BTS7960 Right Motor
 #define R_R_EN 11
 #define R_L_EN 12
 #define R_RPWM 9
@@ -26,28 +27,30 @@
 #define HOR_OFF 'v'
 
 void setup() {
-  // Initialize motor control pins as outputs
+  // Output pins for both motors
+  // left motor
   pinMode(L_R_EN, OUTPUT);
   pinMode(L_L_EN, OUTPUT);
   pinMode(L_RPWM, OUTPUT);
   pinMode(L_LPWM, OUTPUT);
 
+  // Right Motors
   pinMode(R_R_EN, OUTPUT);
   pinMode(R_L_EN, OUTPUT);
   pinMode(R_RPWM, OUTPUT);
   pinMode(R_LPWM, OUTPUT);
 
   pinMode(WP, OUTPUT);
-  // Initialize Bluetooth communication
+  
   Serial.begin(9600);
 
-  // Stop all motors initially
   stopMotors();
+
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char command = Serial.read(); // Read the Bluetooth command
+    char command = Serial.read(); // Read Bluetooth command
 
     switch (command) {
       case FORWARD:
@@ -122,7 +125,7 @@ void moveBackward()
 
 void turnLeft()
 {
-  // Left motor backward, right motor forward
+
   digitalWrite(L_R_EN, HIGH);
   digitalWrite(L_L_EN, HIGH);
   analogWrite(L_RPWM, 0);
@@ -136,7 +139,7 @@ void turnLeft()
 
 void turnRight()
 {
-  // Left motor forward, right motor backward
+  
   digitalWrite(L_R_EN, HIGH);
   digitalWrite(L_L_EN, HIGH);
   analogWrite(L_RPWM, 255);
@@ -159,13 +162,13 @@ void stopMotors()
 
 void forRig()
 {
-  // Left motor forward
+ 
   digitalWrite(L_R_EN, HIGH);
   digitalWrite(L_L_EN, HIGH);
   analogWrite(L_RPWM, 255);
   analogWrite(L_LPWM, 0);
 
-  // Right motor forward
+ 
   digitalWrite(R_R_EN, HIGH);
   digitalWrite(R_L_EN, HIGH);
   analogWrite(R_RPWM, 0);
@@ -174,7 +177,7 @@ void forRig()
 
 void forLef()
 {
-  // Left motor forward
+  
   digitalWrite(L_R_EN, HIGH);
   digitalWrite(L_L_EN, HIGH);
   analogWrite(L_RPWM, 0);
@@ -189,13 +192,12 @@ void forLef()
 
 void bacRig()
 {
-  // Left motor backward
+  
   digitalWrite(L_R_EN, HIGH);
   digitalWrite(L_L_EN, HIGH);
   analogWrite(L_RPWM, 0);
   analogWrite(L_LPWM, 255);
 
-  // Right motor backward
   digitalWrite(R_R_EN, HIGH);
   digitalWrite(R_L_EN, HIGH);
   analogWrite(R_RPWM, 0);
@@ -204,24 +206,28 @@ void bacRig()
 
 void bacLef()
 {
-  // Left motor backward
+
   digitalWrite(L_R_EN, HIGH);
   digitalWrite(L_L_EN, HIGH);
   analogWrite(L_RPWM, 0);
   analogWrite(L_LPWM, 0);
 
-  // Right motor backward
   digitalWrite(R_R_EN, HIGH);
   digitalWrite(R_L_EN, HIGH);
   analogWrite(R_RPWM, 0);
   analogWrite(R_LPWM, 255);
 }
 
+
+// Weapon Motor High and Low Command
+
+// High Command
 void WP_ON()
 {
   digitalWrite(WP, HIGH);
 }
 
+// Low Command
 void WP_OFF()
 {
   digitalWrite(WP, LOW);
